8c2dfc9c386a78cc19a8b2b7673e1fa4cf1993c3
Jason Balladares <Jason.Balladares001@mymdc.net>
2018-09-15 19:22:05 -0400

    python thing works with google cloud

--- src/test.py
+++ src/test.py
@@ -1,11 +1,18 @@
 import speech_recognition as sr
 import sys
+import os
 
 r = sr.Recognizer()
+
+with open("/Users/tvvizzlemac/ShellHacks/shellhacks-2018/src/cred.json") as f:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+
 with sr.Microphone() as source:
+    print("talk")
     audio = r.listen(source)
     try:
-        text = r.recognize_google(audio)
+        text = r.recognize_google_cloud(audio,GOOGLE_CLOUD_SPEECH_CREDENTIALS)
         print("{}".format(text))
     except:
+        print("not working")
         sys.exit(-1)
