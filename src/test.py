66dd3076a417f026564390971e6a0c7d2512bd3f
Jason Balladares <Jason.Balladares001@mymdc.net>
2018-09-15 21:49:36 -0400

    changed a line

--- src/test.py
+++ src/test.py
@@ -4,15 +4,13 @@ import os
 
 r = sr.Recognizer()
 
-with open(os.curdir + "/src/cred.json") as f:
+with open(os.curdir + "/ShellHacks/shellhacks-2018/src/cred.json") as f:
     GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
 
 with sr.Microphone() as source:
-    print("talk")
     audio = r.listen(source)
     try:
         text = r.recognize_google_cloud(audio,GOOGLE_CLOUD_SPEECH_CREDENTIALS)
         print("{}".format(text))
     except:
-        print("not working")
         sys.exit(-1)
